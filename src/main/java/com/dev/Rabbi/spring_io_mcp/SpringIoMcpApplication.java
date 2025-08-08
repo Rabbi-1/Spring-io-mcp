8c842289e1dff7f7efcb83629161e43d060e196c
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-07 23:03:57 -0400

    Set up bean

--- src/main/java/com/dev/Rabbi/spring_io_mcp/SpringIoMcpApplication.java
+++ src/main/java/com/dev/Rabbi/spring_io_mcp/SpringIoMcpApplication.java
@@ -1,13 +1,23 @@
 package com.dev.Rabbi.spring_io_mcp;
 
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringIoMcpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIoMcpApplication.class, args);
+	}
+
+	@Bean
+	public List<ToolCallback> springIOSessionTools(SessionTools sessionTools) {
+		return List.of(ToolCallbacks.from(sessionTools));
 	}
 
 }
